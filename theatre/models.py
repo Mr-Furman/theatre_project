d91d90d84271d9d7f67f1cb3939280f219cdb577
Rostyslav <rostyslav.furman@gmail.com>
2023-08-13 19:09:33 +0300

    first_model, base user

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
